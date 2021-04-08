32d1ba969c67038ba891b965bcca3b71be3ba3b8
Leeky <rbdus7174@naver.com>
2021-04-08 14:50:18 +0900

    .

--- src/main/java/com/liveamonth/liveamonth/LiveAmonthApplication.java
+++ src/main/java/com/liveamonth/liveamonth/LiveAmonthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LiveAmonthApplication {
-
+	//aasd
 	public static void main(String[] args) {
 		SpringApplication.run(LiveAmonthApplication.class, args);
 	}
