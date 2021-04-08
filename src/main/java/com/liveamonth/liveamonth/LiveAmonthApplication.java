c6550623df3a36f04722cfabf1a76e2f3142e1fa
Leeky <rbdus7174@naver.com>
2021-04-08 14:40:08 +0900

    Initial commit

---
+++ src/main/java/com/liveamonth/liveamonth/LiveAmonthApplication.java
@@ -0,0 +1,13 @@
+package com.liveamonth.liveamonth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveAmonthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveAmonthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liveamonth/liveamonth/LiveAmonthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveamonth.liveamonth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveAmonthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
