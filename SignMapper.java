72fb9597326d9f20299182caed38814a4a915c4a
dbqlssla1 <48821334+dbqlssla1@users.noreply.github.com>
2021-04-19 13:38:06 +0900

    Add files via upload

---
+++ SignMapper.java
@@ -0,0 +1,25 @@
+package com.liveamonth.liveamonth.model.mapper.signMapper;
+
+import java.util.HashMap;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.liveamonth.liveamonth.entity.vo.UserVO;
+
+@Repository
+@Mapper
+public interface SignMapper {
+    String checkID(String userID) throws Exception;
+
+    String checkNickName(String userNickName) throws Exception;
+
+    void insertUser(UserVO userVO) throws Exception;
+
+    String checkSign(HashMap<String, Object> hash) throws Exception;
+
+    String findID(String userEmail) throws Exception;
+
+    String findPW(HashMap<String, Object> hash) throws Exception;
+
+}
