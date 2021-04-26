338ff58148cacce2fd7781280c5d1766afb3288e
Leeky <rbdus7174@naver.com>
2021-04-26 14:25:30 +0900

    Merge Developer Code

--- src/main/java/com/liveamonth/liveamonth/controller/signController/SignController.java
+++ src/main/java/com/liveamonth/liveamonth/controller/signController/SignController.java
@@ -58,6 +58,8 @@ public class SignController {
         String userID = request.getParameter(USER_ID.getText());
         String userPassword = request.getParameter(USER_PASSWORD.getText());
         String userName = signService.checkSign(userID, userPassword);
+        //userNO를 사용하기 위한 Code -- userID로 하는건 비효율적. 매번 SQL문을 통해 NO를 알아내야 함.
+        int userNO = signService.checkSign2(userID, userPassword);
 
         if (userName == null) {
             this.firstIn = false;
@@ -67,12 +69,9 @@ public class SignController {
             HttpSession session = request.getSession();
             session.setAttribute(USER_ID.getText(), userID);
             session.setAttribute(USER_NAME.getText(), userName);
+            //userNO를 사용하기 위한 Code
 
-            List<CityInfoVO> cityIntroList = cityInfoService.getCityInfoListByCategory(CATEGORY_INTRO.getCategoryUppercase());
-            List<String> cityNameList = cityInfoService.getCityInfoNameList();
-
-            model.addAttribute(CITY_NAME_LIST.getText(), cityNameList);
-            model.addAttribute(CITY_INTRO_LIST.getText(), cityIntroList);
+            session.setAttribute(USER_NO.getText(), userNO);
             return MAIN.getPath();
         }
     }
